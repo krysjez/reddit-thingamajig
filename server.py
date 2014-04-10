53df2e4194e1b93dcd43abd2768f13413ef1850e
kevin91liu <kevin91liu@gmail.com>
2014-04-10 02:07:42 -0400

    More Python database example

--- server.py
+++ server.py
@@ -2,10 +2,30 @@ import pg8000
 
 #jessica's username is jessicayang, and her password is testing09876
 conn = pg8000.connect(user="michaelnestler", password="testing54321", database="thingamajig", host="cs437dbinstance.cpa1yidpzcc3.us-east-1.rds.amazonaws.com")
-#note it is probable that when you try to run this python script, it fails for you because your IP address is not in the AWS security group, which defines which ip addresses can access the database from outside. if this happens to you, let me (kevin) know. i need to figure out the range of possible ip addresses we will be accessing the database from. i'm not sure if i want to let all ip addresses be able to access. if i do that, outsiders still wouldn't be able to log in though.
+
+conn.autocommit = True #by default this is false. if it is false, then when you change the database (such as insert, update) then you have to do conn.commit() afterwards for the changes to be kept. 
 
 cursor = conn.cursor()
-cursor.execute("select * from public.test;")
-results = cursor.fetchall()
 
-print(results)
+#an example of how to insert values. note that if the primary key already exists in the table, instead of your insert being ignored, you will get an exception. thus, it is necessary to test whether something already exists in the table... and this might be costly
+#cursor.execute("insert into test values (10100, 'Debayan','Gupta');")
+
+#so instead, we can check whether a record already exists before inserting it. 
+cursor.execute("select * from test where \"StudentID\"=10101;")
+if (cursor.rowcount == 0):
+	cursor.execute("insert into test values (10101, 'Eli', 'Yale')")
+
+#example of select
+cursor.execute("select * from test where \"StudentID\"=10101;")
+#cursor.execute("select * from test where StudentID=10100;") does not work. you must have the quotes around StudentID
+results = cursor.fetchall()
+for row in results:
+	SID, firstname, lastname = row
+	print("SID = %d, firstname = %s, lastname = %s" % (SID, firstname, lastname))
+
+#print(results)
+
+
+
+cursor.close()
+conn.close()
