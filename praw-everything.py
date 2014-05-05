23c4ff83961ba5af5bffdceb15eef00959d270f2
Jessica Yang <jessica@jessicayang.org>
2014-05-04 23:35:36 -0400

    put scores back in comment table

--- praw-everything.py
+++ praw-everything.py
@@ -3,6 +3,7 @@
 # To be integrated with Kevin's functions
 
 import praw
+import re
 import datetime
 import pg8000
 
@@ -29,16 +30,16 @@ def upsert_to_subreddits(subredditname, subscribers):
 	sqlstr2 = "insert into public.\"Subreddits\" (\"SubredditName\", \"Subscribers\", \"TimeRecorded\") select '"+subredditname+"', "+str(subscribers)+", now() where not exists (select 1 from public.\"Subreddits\" where \"SubredditName\" = '"+subredditname+"');"
 	cursor.execute(sqlstr2)
 
-def upsert_to_submissions(submissionid, permalink, upvotes, downvotes, title, linkto, selftext, timesubmitted, subredditname, enthusiasmscore, profanityscore):
-	sqlstr1 = "update public.\"Submissions\" set \"Permalink\" = '"+permalink+"', \"Upvotes\" = "+str(upvotes)+", \"Downvotes\" = "+str(downvotes)+", \"Title\" = '"+title+"', \"LinkTo\" = '"+linkto+"', \"SelfText\" = '"+selftext+"', \"TimeSubmitted\" = to_timestamp("+str(timesubmitted)+"), \"SubredditName\" = '"+subredditname+"', \"TimeRecorded\" = (select now()), \"EnthusiasmScore\" = "+str(enthusiasmscore)+", \"ProfanityScore\" = "+str(profanityscore)+" where \"SubmissionID\" = '"+submissionid+"';"
+def upsert_to_submissions(submissionid, permalink, upvotes, downvotes, title, linkto, selftext, timesubmitted, subredditname):
+	sqlstr1 = "update public.\"Submissions\" set \"Permalink\" = '"+permalink+"', \"Upvotes\" = "+str(upvotes)+", \"Downvotes\" = "+str(downvotes)+", \"Title\" = '"+title+"', \"LinkTo\" = '"+linkto+"', \"SelfText\" = '"+selftext+"', \"TimeSubmitted\" = to_timestamp("+str(timesubmitted)+"), \"SubredditName\" = '"+subredditname+"', \"TimeRecorded\" = (select now()) where \"SubmissionID\" = '"+submissionid+"';"
 	cursor.execute(sqlstr1)
-	sqlstr2 = "insert into public.\"Submissions\" (\"SubmissionID\", \"Permalink\", \"Upvotes\", \"Downvotes\", \"Title\", \"LinkTo\", \"SelfText\", \"TimeSubmitted\", \"SubredditName\", \"TimeRecorded\", \"EnthusiasmScore\", \"ProfanityScore\") select '"+submissionid+"', '"+permalink+"', "+str(upvotes)+", "+str(downvotes)+", '"+title+"', '"+linkto+"', '"+selftext+"', to_timestamp("+str(timesubmitted)+"), '"+subredditname+"', now(), "+str(enthusiasmscore)+", "+str(profanityscore)+" where not exists (select 1 from public.\"Submissions\" where \"SubmissionID\" = '"+submissionid+"');"
+	sqlstr2 = "insert into public.\"Submissions\" (\"SubmissionID\", \"Permalink\", \"Upvotes\", \"Downvotes\", \"Title\", \"LinkTo\", \"SelfText\", \"TimeSubmitted\", \"SubredditName\", \"TimeRecorded\") select '"+submissionid+"', '"+permalink+"', "+str(upvotes)+", "+str(downvotes)+", '"+title+"', '"+linkto+"', '"+selftext+"', to_timestamp("+str(timesubmitted)+"), '"+subredditname+"', now() where not exists (select 1 from public.\"Submissions\" where \"SubmissionID\" = '"+submissionid+"');"
 	cursor.execute(sqlstr2)
 
-def upsert_to_comments(commentid, permalink, submissionid, upvotes, downvotes, commenttext, timesubmitted):
-	sqlstr1 = "update public.\"Comments\" set \"Permalink\" = '"+permalink+"', \"SubmissionID\" = '"+submissionid+"', \"Upvotes\" = "+str(upvotes)+", \"Downvotes\" = "+str(downvotes)+", \"CommentText\" = '"+commenttext+"', \"TimeSubmitted\" = to_timestamp("+str(timesubmitted)+"), \"TimeRecorded\" = (select now()) where \"CommentID\" = '"+commentid+"';"
+def upsert_to_comments(commentid, permalink, submissionid, upvotes, downvotes, commenttext, timesubmitted, enthusiasmscore, profanityscore):
+	sqlstr1 = "update public.\"Comments\" set \"Permalink\" = '"+permalink+"', \"SubmissionID\" = '"+submissionid+"', \"Upvotes\" = "+str(upvotes)+", \"Downvotes\" = "+str(downvotes)+", \"CommentText\" = '"+commenttext+"', \"TimeSubmitted\" = to_timestamp("+str(timesubmitted)+"), \"TimeRecorded\" = (select now()), \"EnthusiasmScore\" = "+str(enthusiasmscore)+", \"ProfanityScore\" = "+str(profanityscore)+" where \"CommentID\" = '"+commentid+"';"
 	cursor.execute(sqlstr1)
-	sqlstr2 = "insert into public.\"Comments\" (\"CommentID\", \"Permalink\", \"SubmissionID\", \"Upvotes\", \"Downvotes\", \"CommentText\", \"TimeSubmitted\", \"TimeRecorded\") select '"+commentid+"', '"+permalink+"', '"+submissionid+"', "+str(upvotes)+", "+str(downvotes)+", '"+commenttext+"', to_timestamp("+str(timesubmitted)+"), now() where not exists (select 1 from public.\"Comments\" where \"CommentID\" = '"+commentid+"');"
+	sqlstr2 = "insert into public.\"Comments\" (\"CommentID\", \"Permalink\", \"SubmissionID\", \"Upvotes\", \"Downvotes\", \"CommentText\", \"TimeSubmitted\", \"TimeRecorded\", \"EnthusiasmScore\", \"ProfanityScore\") select '"+commentid+"', '"+permalink+"', '"+submissionid+"', "+str(upvotes)+", "+str(downvotes)+", '"+commenttext+"', to_timestamp("+str(timesubmitted)+"), now(), "+str(enthusiasmscore)+", "+str(profanityscore)+" where not exists (select 1 from public.\"Comments\" where \"CommentID\" = '"+commentid+"');"
 	cursor.execute(sqlstr2)
 
 def upsert_to_users(username, linkkarma, commentkarma, timejoined):
@@ -69,20 +70,28 @@ for submission in subreddit.get_new(limit=MAX_SUBMISSIONS):
 	#submission.replace_more_comments(limit=None, threshold=0)
 	flat_comments = praw.helpers.flatten_tree(submission.comments)
 		for comment in flat_comments:
-			insultingPosts = 0
+			badWordCount = 0
+			wordCount = 0
+			CommentText = comment.body
 			if not (isinstance(comment, praw.objects.MoreComments)) and any(string in comment.body.lower() for string in badWords):
         		insultingPosts = insultingPosts + 1
 			enthu += CommentText.count('?') + CommentText.count('!') + sum(x.isupper() for x in CommentText)
 			lower += sum(x.islower() for x in CommentText)
-			upsert_to_comments(comment.id, comment.permalink, comment._submission.id, comment.ups, comment.downs, comment.body, comment.created_utc)
+			EnthusiasmScore = enthu+1/float(lower+1)
+			for w in re.findall(r"\w+", CommentText):
+    			if w in badWords:
+        			badWordCount+=1
+        		wordCount+=1
+			ProfanityScore = badWords+1/float(wordCount+1)
+			upsert_to_comments(comment.id, comment.permalink, comment._submission.id, comment.ups, comment.downs, comment.body, comment.created_utc, EnthusiasmScore, ProfanityScore)
 			# User who posted the comment
 			user = r.get_redditor(comment.author)
 			upsert_to_users(redditor.name, redditor.link_karma, redditor.comment_karma, redditor.created_utc)
 			insert_to_user_commented(redditor.name, comment.id) # Create user_commented entry
-	EnthusiasmScore = enthu+1/float(lower+1)
+	
 	ProfanityScore = insultingPosts/float(submission.num_comments+1)
 	# User who posted the submission
 	user = r.get_redditor(submission.author)
 	upsert_to_users(redditor.name, redditor.link_karma, redditor.comment_karma, redditor.created_utc)
 	insert_to_user_submitted(redditor.name, submission.id) # Create user_submitted entry
-	upsert_to_submissions(submission.id, submission.permalink, submission.ups, submission.downs, submission.title, submission.url, submission.selftext, submission.created_utc, subreddit.display_name, EnthusiasmScore, ProfanityScore)
+	upsert_to_submissions(submission.id, submission.permalink, submission.ups, submission.downs, submission.title, submission.url, submission.selftext, submission.created_utc, subreddit.display_name)
