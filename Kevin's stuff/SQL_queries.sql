ac7ecd673b75b75ab2209df994f62f17ead5599a
kevin91liu <kevin91liu@gmail.com>
2014-05-04 23:23:26 -0400

    moved fields back
    moved EnthusiasmScore and ProfanityScore back into Comments

--- Kevin's stuff/SQL_queries.sql
+++ Kevin's stuff/SQL_queries.sql
@@ -12,12 +12,78 @@ from
 ) as t1
 natural join
 (
-	select "SubredditName", "Subscribers"
+	select "SubredditName"
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
 order by TriggerHappiness desc
 ;
 
---most profane subreddits
---gives the subreddits with the highest average profanity score in the comments
+--Most profane subreddits
+--Gives the subreddits with the highest average profanity score in the submissions. Each submission has a profanity score, which is calculated from its comments. Also, cut off subreddits with fewer than 100 subscribers.
+select "SubredditName", AvgProfanityScore
+(
+	select "SubredditName", avg(ProfanityScore) as AvgProfanityScore
+	from "Submissions"
+	group by "SubredditName"
+) as t1
+natural join
+(
+	select "SubredditName"
+	from "Subreddits"
+	where "Subscribers" >= 100
+) as t2
+order by AvgProfanityScore desc
+;
+
+--Most enthusiastic subreddits
+--Gives subreddits that use a lot of uppercase characters and exclamation marks
+select "SubredditName", AvgEnthusiasmScore
+(
+	select "SubredditName", avg(EnthusiasmScore) as AvgEnthusiasmScore
+	from "Submissions"
+	group by "SubredditName"
+) as t1
+natural join
+(
+	select "SubredditName"
+	from "Subreddits"
+	where "Subscribers" >= 100
+) as t2
+order by AvgEnthusiasmScore desc
+;
+
+--Nicest subreddits
+--Subreddits with highest average ratio of upvotes to downvotes in its submissions
+select "SubredditName", Niceness
+from
+(
+	select "SubredditName", avg("Upvotes"/"Downvotes") as Niceness
+	from "Submissions"
+	group by "SubredditName"
+) as t1
+natural join
+(
+	select "SubredditName"
+	from "Subreddits"
+	where "Subscribers" >= 100
+) as t2
+order by Niceness desc
+;
+
+--Meanest subreddits
+--Subreddits with the lowest average ratio of upvotes to downvotes in its submissions. Same query as the "Niceness" measure, just flip the order.
+select "SubredditName", Niceness
+from
+(
+	select "SubredditName", avg("Upvotes"/"Downvotes") as Niceness
+	from "Submissions"
+	group by "SubredditName"
+) as t1
+natural join
+(
+	select "SubredditName"
+	from "Subreddits"
+	where "Subscribers" >= 100
+) as t2
+order by Niceness asc
