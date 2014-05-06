d8c979020ebc5272076ca95cbeed8a72c11ffcfa
kevin91liu <kevin91liu@gmail.com>
2014-05-05 20:57:53 -0400

    more fixes to SQL statements

--- Kevin's stuff/SQL_queries.sql
+++ Kevin's stuff/SQL_queries.sql
@@ -3,10 +3,10 @@
 
 --Most trigger-happy subreddits
 --This will give the top subreddits based on the average total votes per post, normalized by the number of subscribers. We normalize by the number of subscribers because otherwise, we'll simply get the most active subreddits, like r/worldnews and r/politics. Also, we cut off subreddits that are too small, with fewer than 100 subscribers. 
-select "SubredditName", AvgVotes / ("Subscribers"+1) as TriggerHappiness
+select "SubredditName", "AvgVotes" / ("Subscribers"+1) as "TriggerHappiness"
 from
 (
-	select "SubredditName", avg("Upvotes"+"Downvotes") as AvgVotes
+	select "SubredditName", avg("Upvotes"+"Downvotes") as "AvgVotes"
 	from "Submissions"
 	group by "SubredditName"
 ) as t1
@@ -16,15 +16,15 @@ natural join
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
-order by TriggerHappiness desc
+order by "TriggerHappiness" desc
 ;
 
 --Most profane subreddits
 --Gives the subreddits with the highest average profanity score in the submissions. Each submission has a profanity score, which is calculated from its comments. Also, cut off subreddits with fewer than 100 subscribers.
-select "SubredditName", AvgProfanityScore
+select "SubredditName", "AvgProfanityScore"
 from
 (
-	select "SubredditName", avg(ProfanityScore) as AvgProfanityScore
+	select "SubredditName", avg("ProfanityScore") as "AvgProfanityScore"
 	from 
 	(
 		select "Comments"."ProfanityScore" as "ProfanityScore", "Submissions"."SubredditName" as "SubredditName"
@@ -38,17 +38,17 @@ natural join
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
-order by AvgProfanityScore desc
+order by "AvgProfanityScore" desc
 ;
 
 
 
 --Most enthusiastic subreddits
 --Gives subreddits that use a lot of uppercase characters and exclamation marks
-select "SubredditName", AvgEnthusiasmScore
+select "SubredditName", "AvgEnthusiasmScore"
 from
 (
-	select "SubredditName", avg(EnthusiasmScore) as AvgEnthusiasmScore
+	select "SubredditName", avg("EnthusiasmScore") as "AvgEnthusiasmScore"
 	from 
 	(
 		select "Comments"."EnthusiasmScore" as "EnthusiasmScore", "Submissions"."SubredditName" as "SubredditName"
@@ -62,15 +62,15 @@ natural join
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
-order by AvgEnthusiasmScore desc
+order by "AvgEnthusiasmScore" desc
 ;
 
 --Nicest subreddits
 --Subreddits with highest average ratio of upvotes to downvotes in its submissions
-select "SubredditName", Niceness
+select "SubredditName", "Niceness"
 from
 (
-	select "SubredditName", avg("Upvotes"/("Downvotes"+"Upvotes"+1)) as Niceness
+	select "SubredditName", avg("Upvotes"::float/("Downvotes"+"Upvotes"+1)) as "Niceness"
 	from "Submissions"
 	group by "SubredditName"
 ) as t1
@@ -80,15 +80,15 @@ natural join
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
-order by Niceness desc
+order by "Niceness" desc
 ;
 
 --Meanest subreddits
 --Subreddits with the lowest average ratio of upvotes to downvotes in its submissions. Same query as the "Niceness" measure, just flip the order.
-select "SubredditName", Niceness
+select "SubredditName", "Niceness"
 from
 (
-	select "SubredditName", avg("Upvotes"/("Downvotes"+"Upvotes"+1)) as Niceness
+	select "SubredditName", avg("Upvotes"::float/("Downvotes"+"Upvotes"+1)) as "Niceness"
 	from "Submissions"
 	group by "SubredditName"
 ) as t1
@@ -98,13 +98,13 @@ natural join
 	from "Subreddits"
 	where "Subscribers" >= 100
 ) as t2
-order by Niceness asc
+order by "Niceness" asc
 
 --Most profane users, based on their comments. Only users with at least 10 comments are considered. 
-select "Username", AvgProfanityScore
+select "Username", "AvgProfanityScore"
 from
 (
-	select "Username", avg(ProfanityScore) as AvgProfanityScore
+	select "Username", avg("ProfanityScore") as "AvgProfanityScore"
 	from ("Users" natural join "User_commented") join "Comments" using ("CommentID")
 	group by "Username"
 ) as t1
@@ -119,14 +119,14 @@ natural join
 	)
 	where "CommentCount" >= 10
 ) as t2
-order by AvgProfanityScore desc
+order by "AvgProfanityScore" desc
 ;
 
 --Most enthusiastic users, based on their comments. Only users with at least 
-select "Username", AvgEnthusiasmScore
+select "Username", "AvgEnthusiasmScore"
 from
 (
-	select "Username", avg(EnthusiasmScore) as AvgEnthusiasmScore
+	select "Username", avg("EnthusiasmScore") as AvgEnthusiasmScore
 	from ("Users" natural join "User_commented") join "Comments" using ("CommentID")
 	group by "Username"
 ) as t1
@@ -141,5 +141,5 @@ natural join
 	)
 	where "CommentCount" >= 10
 ) as t2
-order by AvgEnthusiasmScore desc
+order by "AvgEnthusiasmScore" desc
 ;
